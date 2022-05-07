51843483e38624ba9bd5bcb5bc7d46444ed96567
takenX10 <frau.alessandro01@gmail.com>
2022-05-07 12:45:26 +0200

    implemented scorrimento

--- client/BoatSweetBoat/src/pages/EsploraImbarcazione/Scorrimento/Scorrimento.jsx
+++ client/BoatSweetBoat/src/pages/EsploraImbarcazione/Scorrimento/Scorrimento.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Parallax } from 'react-scroll-parallax';
+
 import Navbar from "@/layouts/Navbar";
 import Footer from "@/layouts/Footer";
 
@@ -6,19 +8,17 @@ import "./Scorrimento.scss"
 
 const Card = ({description, position, imageSource, imageAlt}) => {
     return (
-      <div className="container-fluid card-container d-flex justify-content-center p-0 m-4">
-        <div className="row">
-          <div className="col-12">
-            <img src={imageSource} alt={imageAlt} className='card-image img-fluid'/>
-          </div>
-          <div className="col-12 pt-5 px-4">
-            <h2 className="card-position">{position}</h2>
-          </div>
-          <div className="col-12 pt-1 px-4">
-            <p className='card-description'>{description}</p>
-          </div>
+      <div className="container-fluid card-container d-flex justify-content-center p-0 m-0">
+            <div className="row justify-content-center">
+                <div className="col-12 card-image" style={{backgroundImage:`url(${imageSource})`}}></div>
+                <Parallax translateY={[-20, 20]}>
+                    <div className="col-10 pt-5 px-4 card-position-col justify-content-center align-items-start">
+                        <h2 className="card-position fs-1 text-underline my-5">{position}</h2>
+                        <p className='card-description mb-5 pb-5'>{description}</p>
+                    </div>
+                </Parallax>
+            </div>
         </div>
-      </div>
     );
   }
 
@@ -26,13 +26,8 @@ export default function Scorrimento({cardList}){
     return (
       <>
         <Navbar />
-        <div className="container-fluid">
-          <div className="row justify-content-center m-4">
-            <div className="col-12 m-5 p-3" role="separator"></div>
-            <div className="col-lg-8 col-md-12 col-12">
-              {cardList.map(card => <Card description={card.description} key={card.position} position={card.position} imageSource={card.imageSource} imageAlt={card.imageAlt} />)}
-            </div>
-          </div>
+        <div className="container-fluid cards-box mt-5 pt-5 mx-0 mb-0 px-0 pb-0">
+            {cardList.map(card => <Card description={card.description} key={card.position} position={card.position} imageSource={card.imageSource} imageAlt={card.imageAlt} />)}
         </div>
         <Footer />
       </>
